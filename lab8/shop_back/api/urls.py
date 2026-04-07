f4b2231b2e7926e11c238f16dfe123d0ef116192
aslzhanm <asylzhancode@gmail.com>
2026-04-07 09:25:38 +0500

    lab10

--- lab8/shop_back/api/urls.py
+++ lab8/shop_back/api/urls.py
@@ -9,11 +9,27 @@ from django.urls import path
 #     path('categories/<int:id>/products/', products_of_category),
 # ]
 
-from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ProductViewSet
+# lab9
 
-router = DefaultRouter()
-router.register(r'categories', CategoryViewSet)
-router.register(r'products', ProductViewSet)
+# from rest_framework.routers import DefaultRouter
+# from .views import CategoryViewSet, ProductViewSet
 
-urlpatterns = router.urls
+# router = DefaultRouter()
+# router.register(r'categories', CategoryViewSet)
+# router.register(r'products', ProductViewSet)
+
+# urlpatterns = router.urls
+
+# lab10
+
+from api.views10.generics import *
+
+urlpatterns = [
+    path('products/', ProductListAPIView.as_view()),
+    path('products/<int:product_id>/', ProductDetailAPIView.as_view()),
+
+    path('categories/', CategoryListAPIView.as_view()),
+    path('categories/<int:pk>/', CategoryDetailAPIView.as_view()),
+
+    path('categories/<int:pk>/products/', CategoryProductsAPIView.as_view()),
+]
---
+++ lab8/shop_back/api/views10/cbv.py
@@ -0,0 +1,36 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from api.models import Product
+from api.serializers import ProductSerializer
+
+class ProductListAPIView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        return Response(ProductSerializer(products, many=True).data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+class ProductDetailAPIView(APIView):
+    def get_object(self, product_id):
+        return Product.objects.get(id=product_id)
+
+    def get(self, request, product_id):
+        product = self.get_object(product_id)
+        return Response(ProductSerializer(product).data)
+
+    def put(self, request, product_id):
+        product = self.get_object(product_id)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+    def delete(self, request, product_id):
+        product = self.get_object(product_id)
+        product.delete()
+        return Response()
